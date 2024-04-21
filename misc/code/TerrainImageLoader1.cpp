c03c8a99d18ab2993c98803301e6a58d499b9294
Boonk3312 <theboonk7@hotmail.com>
2024-04-21 14:17:37 -0700

    Create TerrainImageLoader1.cpp

---
+++ misc/code/TerrainImageLoader1.cpp
@@ -0,0 +1,36 @@
+public void imageloader() {
+    file = "terrain_wood.png" => PNG => ImageQuality.[INT](return original);
+    static class yield_return() {
+        int yield = char, const;
+        return yield_return;
+        return yield;
+    }
+    yield_return(0x0000e45ab6);
+    if (yield_return == 0x0000e45ab6) {
+        yield_return(1)::Imagefile<PNG>;
+    }
+    #define DataTags.Index() => {
+        return 01;
+        private static void Index() {
+            yield_return(connect);
+            int connect.ImageConnectionToImageLoader();
+            {
+                return 3;
+                int connection.Type()::const c_str = "index";
+                static int imageconnection();
+                {
+                    static int $Tag => () {
+                        if (TAG == x1 || > %int% > calculation:#int bit = 0156, {goto __c_tag__});
+                        static void TagClass();
+                        {
+                            #define tagclass => {
+                               private static class #define TagClass = const;   
+                            };
+                        }
+                        new TagClass("__c_tag__", Tag@ImageConnection:imagefile(png)?SelectedImage<PushConnection>)
+                    }
+                }
+            }
+        }
+    }
+}
